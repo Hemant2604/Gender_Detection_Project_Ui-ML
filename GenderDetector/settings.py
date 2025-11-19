6ecca261616a5212b95f8f01e49ff2cbcb3c31bc
Hemant2604 <hemantchauthe577@gmail.com>
2025-11-19 16:09:29 +0530

    repair

--- GenderDetector/settings.py
+++ GenderDetector/settings.py
@@ -22,7 +22,11 @@ SECRET_KEY = 'django-insecure-your-secret-key-change-in-production'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['gender-detection-project-ui-ml.onrender.com', 'localhost']
+ALLOWED_HOSTS = [
+    'gender-detection-project-ui-ml.onrender.com',
+    'localhost',
+    '127.0.0.1',
+]
 
 
 
